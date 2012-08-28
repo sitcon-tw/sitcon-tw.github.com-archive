f7db1f00967a5487dd0ee52292fe546c3b30886e
蒼時弦也 <elct9620@frost.tw>
2012-08-28 12:59:36 +0800

    修正手機連結點選問題

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,16 @@
 /*jslint browser: true*/
-/*global jQuery,console*/
+/*global jQuery,console,Modernizr*/
 
 (function ($) {
     "use strict";
 
     $(function () {
+
+        // Mobile Link Fix
+        if (!Modernizr.touch) {
+            $('#facebook a, #twitter a, #survey a').addClass('has-tip');
+        }
+
         $(document).tooltips();
     });
 
